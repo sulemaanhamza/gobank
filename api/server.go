97a6d158d638f1ee871de316bebbf112f60bf343
Suleman <sulemanhamza97@gmail.com>
2023-02-28 10:42:21 +0500

    API - createTransferAPI

--- api/server.go
+++ api/server.go
@@ -19,6 +19,8 @@ func NewServer(store db.Store) *Server {
 	router.GET("/accounts/:id", server.getAccount)
 	router.POST("/accounts", server.createAccount)
 
+	router.POST("/transfers", server.createTransfer)
+
 	server.router = router
 	return server
 }
